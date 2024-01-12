246148c534a5bddfdb69c72cecc18722fb1121c8
Rijnder Wever <rien334@gmail.com>
2024-01-12 14:41:02 +0100

    Add alternate shortcut for displaying recent directories

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,6 @@
 function fish_user_key_bindings
     bind \eL 'echo; lsh  --color=always --indicator-style=slash |  sed "s/^/ /" ; commandline -f force-repaint'
+    bind \eh 'echo; lsh  --color=always --indicator-style=slash |  sed "s/^/ /" ; commandline -f force-repaint'
     bind \ev 'true'
     bind \ep 'echo; pwd ; commandline -f force-repaint'
     bind \e\" __fish_commandline_insert_escaped\ \'\"\"\'\;\ commandline\ -f\ backward-char
