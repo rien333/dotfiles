12475814128be886eee5c8506393a972ebfc3ee1
Rijnder Wever <rien334@gmail.com>
2024-08-07 09:54:51 +0200

    update fish and beets

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -1,5 +1,5 @@
 function fish_prompt
-	set_color blue --bold
+    set_color blue --bold
     echo -n (basename (pwd | sed "s|^/home/rw|~|"))
     set_color red
     echo -n "> "
--- fish/functions/zstdtar.fish
+++ fish/functions/zstdtar.fish
@@ -1,14 +1,19 @@
 function zstdtar
     argparse h/help -- $argv
 
+
     # If -h or --help is given, we print a little help text and return
     if set -q _flag_help
-        echo -e "usage: zstdtar [directory]\n\n Decompress a directory when given a tar.zst file, and compress when given a normal directory. Remvove the original archive when unpacked"
+        echo -e "usage: zstdtar [directory]\n\n Decompress a directory when given a tar.zst file, and compress when given a normal directory. Remove the original archive when unpacked"
         return 0
     end
 
-    if string match -q "directory" (file -b $argv[1])
-        env ZSTD_NBTHREADS=9 ZSTD_CLEVEL=5 tar --zstd -cf $argv[1].tar.zst $argv[1]
+    set fname (echo $argv[1] | sd '(.*)/$' '$1')
+
+    if file -b $fname | grep -q "directory"
+        set archive_name "$fname".tar.zst
+        touch "$archive_name"
+        env ZSTD_NBTHREADS=9 ZSTD_CLEVEL=5 tar --exclude "$archive_name" --zstd -cf "$archive_name" "$fname"
     else if string match -q -r '.*\.tar\.zst' $argv[1]
         env ZSTD_NBTHREADS=9 tar --zstd -xvf $argv[1]
         rm $argv[1]
