9790bb601db9ec42809b6366cbbe6e3e1e761963
Rijnder Wever <rien334@gmail.com>
2025-04-15 22:22:58 +0200

    fish: fix keyboard shortcuts on 4.X version

--- fish/functions/fish_user_key_bindings.fish
+++ fish/functions/fish_user_key_bindings.fish
@@ -3,8 +3,8 @@ function fish_user_key_bindings
     bind \eh 'echo; lsh  --color=always --indicator-style=slash |  sed "s/^/ /" ; commandline -f force-repaint'
     bind \ev 'true'
     bind \ep 'echo; pwd ; commandline -f force-repaint'
-    bind \e\" __fish_commandline_insert_escaped\ \'\"\"\'\;\ commandline\ -f\ backward-char
-    bind \e\' __fish_commandline_insert_escaped\ \"\'\'\"\;\ commandline\ -f\ backward-char
-    bind \e\( __fish_commandline_insert_escaped\ \\\(\\\)\;\ commandline\ -f\ backward-char\ force-repaint
+    bind alt-'"' 'commandline -i \"\"; commandline -f backward-char'
+    bind alt-"'" "commandline -i \'\'; commandline -f backward-char"
+    bind alt-"(" "commandline -i \(\); commandline -f backward-char"
     # bind \ed 'commandline -f kill-line; fish_clipboard_copy $killring[1]; commandline -f force-repaint'
 end
